16a433ca0a8dfb1f93b5fe9c715148db0ac46d88
MovingEmoji <mcnana814@gmail.com>
2024-08-25 14:54:37 +0900

    Initial commit

---
+++ src/main/java/jp/nagua/mobile_order/MobileOrderApplication.java
@@ -0,0 +1,13 @@
+package jp.nagua.mobile_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileOrderApplication.class, args);
+	}
+
+}
