b3040771d017030e10703bc438c7964b31ad3980
MovingEmoji <mcnana814@gmail.com>
2024-08-30 00:54:40 +0900

    ステータスを変更できるようにした

---
+++ src/main/java/jp/nagua/mobile_order/controllers/UpdateStatus.java
@@ -0,0 +1,31 @@
+package jp.nagua.mobile_order.controllers;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import jp.nagua.mobile_order.MobileOrderApplication;
+import jp.nagua.mobile_order.elements.OrderContent;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
+
+@Controller
+public class UpdateStatus {
+
+    @PostMapping(value = "/update")
+    @ResponseBody
+    public String update(@RequestBody String str) {
+        JsonObject json = JsonParser.parseString(str).getAsJsonObject();
+        UUID uuid = UUID.fromString(json.get("uuid").getAsString());
+        String status =json.get("status").getAsString();
+        for(OrderContent order : MobileOrderApplication.orders) {
+            if(order.getOrder_id().toString().equals(uuid.toString())) {
+                order.setStatus(status);
+                return "success";
+            }
+        }
+        return "failed";
+    }
+}
