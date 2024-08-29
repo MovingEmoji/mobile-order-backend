6523df8ba6676c1898c4254f1c8bb2fbbb05eab5
MovingEmoji <mcnana814@gmail.com>
2024-08-30 00:54:04 +0900

    準備中リストを返す実装

--- src/main/java/jp/nagua/mobile_order/controllers/PendingList.java
+++ src/main/java/jp/nagua/mobile_order/controllers/PendingList.java
@@ -2,10 +2,9 @@ package jp.nagua.mobile_order.controllers;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import jp.nagua.mobile_order.MobileOrderApplication;
 import jp.nagua.mobile_order.elements.OrderContent;
+import jp.nagua.mobile_order.elements.OrderItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +24,30 @@ public class PendingList {
                 HashMap<Object, Object> content = new HashMap<>();
                 content.put("id", order.getOrder_num());
                 content.put("uuid", order.getOrder_id());;
+                int total = 0;
+                int cake = 0;
+                int waffle = 0;
+                int dango = 0;
+                for(OrderItem item : order.getOrder_list()) {
+                    total += item.getItem_cost();
+                    switch (item.getItem_id()) {
+                        case 1:
+                            cake += item.getItem_count();
+                            break;
+                        case 2:
+                            waffle += item.getItem_count();
+                            break;
+                        case 3:
+                            dango += item.getItem_count();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                content.put("cake", cake);
+                content.put("waffle", waffle);
+                content.put("dango", dango);
+                content.put("totalCost", total);
                 pendings.add(new Gson().toJsonTree(content));
             }
         }
