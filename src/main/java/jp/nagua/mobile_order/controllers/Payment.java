30bdf7a9528d242a63e727561dbe574a4d62a1e6
MovingEmoji <mcnana814@gmail.com>
2024-09-12 15:00:21 +0900

    決済システムの実装とPayPayへの対応

--- src/main/java/jp/nagua/mobile_order/controllers/Payment.java
+++ src/main/java/jp/nagua/mobile_order/controllers/Payment.java
@@ -1,20 +1,25 @@
 package jp.nagua.mobile_order.controllers;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import jp.nagua.mobile_order.MobileOrderApplication;
+import jp.nagua.mobile_order.elements.PaymentData;
 import jp.ne.paypay.ApiClient;
 import jp.ne.paypay.ApiException;
 import jp.ne.paypay.Configuration;
 import jp.ne.paypay.api.PaymentApi;
 import jp.ne.paypay.model.MoneyAmount;
+import jp.ne.paypay.model.PaymentDetails;
 import jp.ne.paypay.model.QRCode;
 import jp.ne.paypay.model.QRCodeDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
@@ -29,19 +34,58 @@ public class Payment {
     @ResponseBody
     public String payment(@RequestBody String str) throws ApiException {
         JsonObject json = JsonParser.parseString(str).getAsJsonObject();
-        ApiClient apiClient = new Configuration().getDefaultApiClient();
-        apiClient.setProductionMode(false);
-        apiClient.setApiKey(API_KEY);
-        apiClient.setApiSecretKey(API_SECRET);
-        apiClient.setAssumeMerchant(MERCHANT_ID);
-        QRCode qrCode = new QRCode();
-        qrCode.setAmount(new MoneyAmount().amount(json.get("total").getAsInt()).currency(MoneyAmount.CurrencyEnum.JPY));
-        qrCode.setMerchantPaymentId(json.get("uuid").getAsString());
-        qrCode.setCodeType("ORDER_QR");
-        qrCode.setOrderDescription("モバイルオーダー支払い");
-        qrCode.isAuthorization(false);
-        PaymentApi apiInstance = new PaymentApi(apiClient);
-        QRCodeDetails response = apiInstance.createQRCode(qrCode);
-        return response.getData().getUrl();
+        PaymentData data = MobileOrderApplication.getPaymentData(json.get("uuid").getAsString());
+        if(json.get("type").getAsString().equals("cash")) {
+            if(MobileOrderApplication.runPayment(data, json.get("deposit").getAsInt()) == true) {
+                Map<Object, Object> jsonMap = new HashMap<>();
+                jsonMap.put("uuid", data.getUUID());
+                jsonMap.put("status", data.getStatus());
+                jsonMap.put("total", data.getTotal());
+                jsonMap.put("deposit", data.getDeposit());
+                jsonMap.put("change", data.getChange());
+                MobileOrderApplication.getOrderContent(data.getUUID().toString()).setStatus("paid");
+                return new Gson().toJson(jsonMap);
+            }
+            return "failed";
+        } else if(json.get("type").getAsString().equals("paypay")) {
+            ApiClient apiClient = new Configuration().getDefaultApiClient();
+            apiClient.setProductionMode(false);
+            apiClient.setApiKey(API_KEY);
+            apiClient.setApiSecretKey(API_SECRET);
+            apiClient.setAssumeMerchant(MERCHANT_ID);
+            QRCode qrCode = new QRCode();
+            qrCode.setAmount(new MoneyAmount().amount(data.getTotal()).currency(MoneyAmount.CurrencyEnum.JPY));
+            qrCode.setMerchantPaymentId(json.get("uuid").getAsString());
+            qrCode.setCodeType("ORDER_QR");
+            qrCode.setOrderDescription("モバイルオーダー支払い");
+            qrCode.isAuthorization(false);
+            qrCode.setRedirectUrl("http://localhost:3000/customer");
+            PaymentApi apiInstance = new PaymentApi(apiClient);
+            QRCodeDetails response = apiInstance.createQRCode(qrCode);
+            data.setStatus("paypayPending");
+            data.setURL(response.getData().getUrl());
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    PaymentApi apiInstance = new PaymentApi(apiClient);
+                    while(true) {
+                        try {
+                            PaymentDetails response = apiInstance.getCodesPaymentDetails(json.get("uuid").getAsString());
+                            if(response.getData().getStatus().toString().equals("COMPLETED")) {
+                                break;
+                            }
+                            Thread.sleep(1000);
+                        } catch (ApiException | InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    MobileOrderApplication.runPayment(data, data.getTotal());
+                    MobileOrderApplication.getOrderContent(data.getUUID().toString()).setStatus("paid");
+                }
+            });
+            thread.start();
+            return response.getData().getUrl();
+        }
+        return "failed";
     }
 }
